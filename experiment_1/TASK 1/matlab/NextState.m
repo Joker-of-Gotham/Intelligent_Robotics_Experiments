951dea8e0e1de4a88c9f7b865e438e02b9b7aeae
Cecile <1438314832@qq.com>
2025-09-12 11:48:18 +0800

    add task1 matlab

---
+++ experiment_1/TASK 1/matlab/NextState.m
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d3b9c94602ac53d7a10db58da99b1a177e201d4a63fe4840dc88e7979bcecb4b
+size 2001
---
+++ experiment_1/TASK 1/matlab/main.m
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e6963c999e6a98c9cfe7f150ec3ed297ab91c211d07432ca1d9643d6df35959f
+size 7505
