1d341f7995e18ebc92a448fc23d9e28e1647835e
Cecile <1438314832@qq.com>
2025-09-12 12:05:15 +0800

    ADD TASK2 CODE

---
+++ experiment_1/matlab_code_and_result/TASK2/TrajectoryGenerator.m
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9475538067f154bb92a5e7fa2b044d686b3cdb9aee57032fa83181250b67a70b
+size 6371
---
+++ experiment_1/matlab_code_and_result/TASK2/main_2.m
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:afed4fc8c4624cb0a0fbd5c1aec52c26a86b2381892cf01dc7407cfde5639b55
+size 1204
